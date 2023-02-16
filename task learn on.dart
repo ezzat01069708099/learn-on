8feceadc6b5bd4a857897ba685bf2b8d39e7ca16
ezzat mohamed <125498048+ezzat01069708099@users.noreply.github.com>
2023-02-16 23:01:48 +0200

    Add files via upload

---
+++ task learn on.dart
@@ -0,0 +1,22 @@
+void main(List<String> arguments) {
+ 
+ var name = "hello i am john" ;
+ print(name);
+
+
+ int p = 5;
+ int t = 10;
+ int r = 2;
+ int x =(p*t*r);
+ print(x);
+
+
+ int f = 5;
+ int g =(f^2);
+ print(g);
+
+
+  String age = "22";
+  int value = int.parse(age);
+   print(value); 
+}
