8ea84fc87ebe8b29476678ab2ba6a30b3d69b8e0
ezzat mohamed <125498048+ezzat01069708099@users.noreply.github.com>
2023-02-24 20:46:26 +0200

    Add files via upload

---
+++ task learn on2.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+
+//1
+ dynamic List = ["red","green","white","black"]; 
+ print(List.first); 
+
+
+//2
+ double L=3.14;
+ int r=6;
+ double x=(L*(r^3)); 
+print(x);
+
+//3 !??
+
+//4
+int List2 = [1,2,3,4];
+print(List2);
+ 
+
+ //5
+int? num=int.parse(stdin.readLineSync()!);
+if(num%2==0);
+  {
+   print("even")
+  }
+  else 
+  {
+    print("ood")
+  }
+}
+
